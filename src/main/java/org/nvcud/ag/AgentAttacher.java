c02a9f51442415cd49a90c68913498272f76261f
MysticalYcc <msiyc110@gmail.com>
2025-10-24 14:29:26 +0800

    1
    1

--- src/main/java/org/nvcud/ag/AgentAttacher.java
+++ src/main/java/org/nvcud/ag/AgentAttacher.java
@@ -2,21 +2,29 @@ package org.nvcud.ag;
 
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
+import org.nvcud.conf.Tim;
 
 import java.lang.management.ManagementFactory;
 
 public class AgentAttacher {
 
     public void main(String path) throws Exception {
-        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-        String mainClass = System.getProperty("sun.java.command");
-        for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
-            if (vmd.displayName().contains(mainClass)) {
-                VirtualMachine vm = VirtualMachine.attach(vmd.id());
-                vm.loadAgent(path);
-                vm.detach();
-                return;
+        try {
+            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+            String mainClass = System.getProperty("sun.java.command");
+            for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
+                if (vmd.displayName().contains(mainClass)) {
+                    VirtualMachine vm = VirtualMachine.attach(vmd.id());
+                    vm.loadAgent(path);
+                    vm.detach();
+                    return;
+                }
             }
+            Tim tim = new Tim();
+            tim.lqcf();
+        }catch (Exception e){
+
         }
+
     }
 }
